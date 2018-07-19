8f09ee9f6ceca238e68492d20df4fb4e1856d06c
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-07-19 14:25:04 +0200

    Some refactoring and corrected bad tests

--- newAirportKataJS/spec/AirportSpec.js
+++ newAirportKataJS/spec/AirportSpec.js
@@ -1,25 +1,42 @@
 'use strict';
 
 describe('Airport', function() {
-   var airport; var plane;
-   beforeEach(function() {airport = new Airport(); plane = new Plane(); });
+    var airport, plane, weather;
 
-   it('has an default capacity', function() {expect(airport.CAPACITY).toEqual(33)});
+    beforeEach(function() {
+       plane = new Plane();
+       weather = new Weather();
+       airport = new Airport(33, weather);
+   });
+
+    it('small thing', function() {
+        spyOn(weather, 'isStormy').and.returnValue(false);
+        expect(weather.isStormy()).toEqual(false);
+    });
+
+    it('has a default capacity', function() { expect(airport.CAPACITY).toEqual(33)});
+
    it('accepts custom capacity on initiation', function () {
        var airport2 = new Airport(100);
        expect(airport2.capacity).toEqual(100)
    });
 
    describe('gives permission to land', function(){
+       beforeEach(function(){
+           spyOn(weather, 'isStormy').and.returnValue(false);
+       });
+
        it('- plane lands successfully', function(){
            spyOn(plane, 'landAtAirport').and.returnValue(true);
            expect(airport.land(plane)).toEqual(true);
            expect(plane.landAtAirport).toHaveBeenCalled();
-        });
+       });
+
        it("- plane's added to the list of landed planes", function(){
            airport.land(plane);
            expect(airport.landedPlanes).toContain(plane);
        });
+
        it('- the availability decreases with each landed plane', function(){
            airport.land(plane); airport.land(plane);
            expect(airport.landedPlanes).toContain(plane, plane);
@@ -28,17 +45,22 @@ describe('Airport', function() {
    });
 
    describe('gives permission to take off', function(){
-       beforeEach(function() {airport.land(plane); airport.land(plane);});
+       beforeEach(function(){
+           spyOn(weather, 'isStormy').and.returnValue(false);
+           airport.land(plane); airport.land(plane);
+       });
 
        it('- plane takes off successfully',function() {
            spyOn(plane, 'takeOff').and.returnValue(false);
            expect(airport.takeOffOk(plane)).toEqual(false);
            expect(plane.takeOff).toHaveBeenCalled();
        });
+
        it("- plane's removed from the list of landed planes", function(){
            airport.takeOffOk(plane);
            expect(airport.landedPlanes).toContain(plane, plane);
        });
+
        it('- the availability grows', function(){
            airport.takeOffOk(plane);
            expect(airport.landedPlanes).toContain(plane);
@@ -46,12 +68,27 @@ describe('Airport', function() {
        });
    });
 
-   describe("doesn't give permission to land", function() {
 
+   describe("doesn't give permission to land", function() {
        it("- when it's full", function(){
+           spyOn(weather, 'isStormy').and.returnValue(false);
            var cap = airport.capacity;
            for(var i = 0; i < cap; i++) {airport.land(plane);}
            expect(function(){ airport.land(plane); }).toThrowError('airport filled to capacity');
        });
+
+       it('- when the weather is stormy', function() {
+           spyOn(weather, 'isStormy').and.returnValue(true);
+
+           expect(function(){ airport.land(plane); }).toThrowError('Stormy weather. Landing denied');
+       })
+   });
+
+   describe("doesn't give permission to take off", function() {
+       it('when the weather is stormy', function() {
+           spyOn(weather, 'isStormy').and.returnValue(true);
+           expect(function() { airport.takeOffOk(plane); }).toThrowError('Stormy weather. Take-off denied');
+           expect(weather.isStormy).toHaveBeenCalled();
+       })
    })
 });
--- newAirportKataJS/src/Airport.js
+++ newAirportKataJS/src/Airport.js
@@ -1,27 +1,37 @@
 'use strict';
 
-function Airport(customCapacity) {
+function Airport(customCapacity, weather) {
     this.landedPlanes = [];
     Object.defineProperty(Airport.prototype,
         'CAPACITY', {
             value: 33
         });
     this.capacity = customCapacity || this.CAPACITY;
+    this._weather =  weather || new Weather();
+
 }
+
+Object.defineProperty(Airport.prototype, 'availability', {
+    get: function () { return this.capacity - this.landedPlanes.length }
+});
 
 Airport.prototype.land = function(plane) {
     if (this._isFull()) {
         throw new Error('airport filled to capacity');
+    } else if (this._weather.isStormy()) {
+        throw new Error('Stormy weather. Landing denied')
     }
     this.landedPlanes.push(plane);
-    this.availability = this.capacity - this.landedPlanes.length;
+    // this.availability = this.capacity - this.landedPlanes.length;
     return plane.landAtAirport();
 };
 
 Airport.prototype.takeOffOk = function(plane) {
+    if (this._weather.isStormy()) {
+        throw new Error('Stormy weather. Take-off denied')
+    }
     var leavingPlane = this.landedPlanes.pop();
-    console.log(this.availability);
-    this.availability = this.capacity - this.landedPlanes.length;
+    // this.availability = this.capacity - this.landedPlanes.length;
     return leavingPlane.takeOff();
 };
 
