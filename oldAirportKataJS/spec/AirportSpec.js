621f356fe7446db564933f72aaee959d4c1de767
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-07-19 14:25:54 +0200

    Tidied up

--- oldAirportKataJS/spec/AirportSpec.js
+++ oldAirportKataJS/spec/AirportSpec.js
@@ -19,10 +19,12 @@ describe('Airport', function(){
     beforeEach(function(){
       weather.isStormy.and.returnValue(false);
     });
+
     it('can clear planes for landing', function(){
       airport.clearForLanding(plane);
       expect(airport.planes()).toEqual([plane]);
     });
+
     it('can clear planes for takeoff', function(){
       airport.clearForLanding(plane);
       airport.clearForTakeOff(plane);
@@ -34,9 +36,11 @@ describe('Airport', function(){
     beforeEach(function(){
       weather.isStormy.and.returnValue(true);
     });
+
     it('does not clear planes for landing', function(){
       expect(function(){ airport.clearForLanding(plane); }).toThrowError('cannot land during storm');
     });
+
     it('does not clear planes for takeoff', function(){
       expect(function(){ airport.clearForTakeOff(plane); }).toThrowError('cannot takeoff during storm');
     });
--- oldAirportKataJS/src/Airport.js
+++ oldAirportKataJS/src/Airport.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function Airport(weather){
-  this._weather = typeof weather !== 'undefined' ? weather : new Weather()
+  this._weather = typeof weather !== 'undefined' ? weather : new Weather();
   this._hangar = [];
 }
 
